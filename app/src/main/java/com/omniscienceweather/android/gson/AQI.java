f4bdeb756844ce23fcdfd9cbdfccc4dbc1824e9a
YW <2496403368@qq.com>
2020-11-27 12:37:04 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/omniscienceweather/android/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.omniscienceweather.android.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
